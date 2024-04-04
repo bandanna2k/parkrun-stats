3c74f51e8f3a06d7a68b540d3d6769989d5cef10
David North <bandanna2k@gmail.com>
2024-04-04 19:15:28 +1300

    parkrun-stats - Dead code.

--- database/src/dnt/parkrun/database/weekly/AthleteCourseSummaryDao.java
+++ database/src/dnt/parkrun/database/weekly/AthleteCourseSummaryDao.java
@@ -2,19 +2,14 @@ package dnt.parkrun.database.weekly;
 
 import dnt.parkrun.common.DateConverter;
 import dnt.parkrun.database.BaseDao;
-import dnt.parkrun.datastructures.Athlete;
 import dnt.parkrun.datastructures.AthleteCourseSummary;
-import dnt.parkrun.datastructures.Country;
-import dnt.parkrun.datastructures.Course;
-import dnt.parkrun.datastructures.stats.RunsAtEvent;
 import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 
 import javax.sql.DataSource;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static dnt.parkrun.datastructures.Country.NZ;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class AthleteCourseSummaryDao extends BaseDao
 {
@@ -83,80 +78,5 @@ public class AthleteCourseSummaryDao extends BaseDao
                         rs.getInt("course_id"),
                         rs.getInt("run_count")
                 });
-    }
-
-    public List<RunsAtEvent> getMostRunsInRegion()
-    {
-        String sql = "select athlete_id, name, course_long_name, run_count\n" +
-                "from " + tableName + // TODO
-                "\n" +
-                "right join course c using (course_long_name)\n" +
-                "right join athlete a using (athlete_id)\n" +
-                "\n" +
-                "where c.country_code = " + NZ.countryCode +
-                "order by run_count desc limit 50;\n";
-        return Collections.emptyList();
-    }
-
-    public List<RunsAtEvent> XgetMostRunsAtEvent()
-    {
-        // TODO Only includes pIndex and Most Events. Not necessarily the max runners.
-        String sql =
-                "select sub1.course_long_name, c.course_id, c.course_name, c.country_code, sub2.athlete_id, sub3.name, sub1.max_run_count\n" +
-                        "from course c\n" +
-                        "left join\n" +
-                        "(\n" +
-                        "    select course_long_name, max(run_count) as max_run_count\n" +
-                        "    from " + tableName + // TODO
-                        "    group by course_long_name\n" +
-                        ") as sub1 using (course_long_name)\n" +
-                        "left join\n" +
-                        "(\n" +
-                        "    select athlete_id, course_long_name, run_count\n" +
-                        "    from athlete_course_summary_2024_03_09\n" +
-                        ") as sub2 on c.course_long_name = sub2.course_long_name and sub1.max_run_count = sub2.run_count\n" +
-                        "left join\n" +
-                        "(\n" +
-                        "    select athlete_id, name\n" +
-                        "    from athlete\n" +
-                        ") as sub3 on sub2.athlete_id = sub3.athlete_id\n" +
-                        "where c.country_code = 65 " +
-                        "order by course_long_name";
-        return jdbc.query(sql, EmptySqlParameterSource.INSTANCE, (rs, rowNum) ->
-        {
-            Athlete athlete = Athlete.from(rs.getString("name"), rs.getInt("athlete_id"));
-            Country country = Country.valueOf(rs.getInt("country_code"));
-            Course course = new Course(
-                    rs.getInt("course_id"),
-                    rs.getString("course_name"),
-                    country,
-                    rs.getString("course_long_name"),
-                    null);
-            return new RunsAtEvent(
-                    athlete,
-                    course,
-                    rs.getInt("max_run_count")
-            );
-        });
-    }
-
-    public void XupdateAthleteCourseSummariesWithVolunteerCounts(Map<Integer, Integer> athleteToVolunteers)
-    {
-        String sql = "update " + tableName + " set " +
-                "global_volunteer_count = ? where athlete_id = ?";
-        List<Object[]> batch = new ArrayList<>();
-        athleteToVolunteers.forEach((athleteId, count) ->
-                batch.add(new Object[] { count, athleteId } ));
-        jdbc.getJdbcTemplate().batchUpdate(sql, batch);
-    }
-
-    public void updateAthleteCourseSummariesWithVolunteerCount(int athleteId, AtomicInteger volunteerCount)
-    {
-        String sql = "update " + tableName + " set " +
-                "global_volunteer_count = :volunteerCount where athlete_id = :athleteId";
-        MapSqlParameterSource params = new MapSqlParameterSource()
-                .addValue("volunteerCount", volunteerCount)
-                .addValue("athleteId", athleteId);
-        jdbc.update(sql, params);
     }
 }
