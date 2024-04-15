76d12c3d8388f8ba7d38a46a6ce735811a2faa88
David North <bandanna2k@gmail.com>
2024-04-15 18:03:41 +1200

    parkrun-stats - WIP MostEvents test.

--- database/src/dnt/parkrun/database/stats/MostEventsDao.java
+++ database/src/dnt/parkrun/database/stats/MostEventsDao.java
@@ -33,21 +33,26 @@ public class MostEventsDao extends BaseDao
     }
     private String getTableName()
     {
+        return getTableName(date);
+    }
+    static String getTableName(Date date)
+    {
         return "most_events_for_region_" + DateConverter.formatDateForDbTable(date);
     }
 
     private void init()
     {
-        String sql =
-                "create table if not exists " + getTableName() + " ( " +
-                "    name                           VARCHAR(255)    NOT NULL," +
-                "    athlete_id                     INT             NOT NULL," +
-                "    different_region_course_count  INT             NOT NULL," +
-                "    total_region_runs              INT             NOT NULL," +
-                "    different_course_count         INT             NOT NULL," +
-                "    total_runs                     INT             NOT NULL," +
-                " PRIMARY KEY (athlete_id) " +
-                ") DEFAULT CHARSET=utf8mb4";
+        String sql = STR."""
+                create table if not exists \{getTableName()} (
+                      athlete_id                     INT             NOT NULL,
+                      name                           VARCHAR(255)    NOT NULL,
+                      different_region_course_count  INT             NOT NULL,
+                      total_region_runs              INT             NOT NULL,
+                      different_course_count         INT             NOT NULL,
+                      total_runs                     INT             NOT NULL,
+                      PRIMARY KEY (athlete_id)
+                ) DEFAULT CHARSET=utf8mb4
+                """;
         jdbc.update(sql, EmptySqlParameterSource.INSTANCE);
     }
 
@@ -58,30 +63,38 @@ public class MostEventsDao extends BaseDao
     {
         try
         {
-            String sql = "insert into " + getTableName() + "(name, athlete_id, different_region_course_count, total_region_runs, different_course_count, total_runs)" +
-                    "select a.name, a.athlete_id, " +
-                    "sub1.count as different_region_course_count, sub2.count as total_region_runs, " +
-                    "0 as different_course_count, 0 as total_runs " +
-                    "from " + athleteTable() + " a " +
-                    "join   " +
-                    "( " +
-                    "    select athlete_id, count(course_id) as count " +
-                    "    from (select distinct athlete_id, course_id from " + resultTable() + ") as sub1a " +
-                    "    group by athlete_id " +
-                    "    having count >= " + MIN_DIFFERENT_REGION_COURSE_COUNT +
-                    "    order by count desc, athlete_id asc  " +
-                    ") as sub1 on sub1.athlete_id = a.athlete_id " +
-                    "join " +
-                    "( " +
-                    "    select athlete_id, count(concat) as count " +
-                    "    from (select athlete_id, concat(athlete_id, '-', course_id, '-', date, '-', position) as concat from " + resultTable() + ") as sub2a " +
-                    "    group by athlete_id " +
-                    "    order by count desc, athlete_id asc  " +
-                    ") as sub2 on sub2.athlete_id = a.athlete_id " +
-                    "where a.name is not null " +
-                    "order by different_region_course_count desc, total_region_runs desc, a.athlete_id desc ";
-
-            jdbc.update(sql, EmptySqlParameterSource.INSTANCE);
+            String sql = STR."""
+                insert into \{getTableName()}(
+                    name, athlete_id, different_region_course_count, total_region_runs,
+                    different_course_count, total_runs)
+                select a.name, a.athlete_id,
+                    sub1.count as different_region_course_count,
+                    sub2.count as total_region_runs,
+                    0 as different_course_count, 0 as total_runs from \{athleteTable()} a
+                join (
+                    select athlete_id, count(course_id) as count
+                    from (
+                        select distinct athlete_id, course_id
+                        from \{resultTable()}
+                    ) as sub1a
+                    group by athlete_id
+                    having count >= :minDifferentRegionCourseCount
+                    order by count desc, athlete_id asc
+                ) as sub1 on sub1.athlete_id = a.athlete_id
+                join
+                (
+                    select athlete_id, count(concat) as count
+                    from (
+                        select athlete_id, concat(athlete_id, '-', course_id, '-', date, '-', position) as concat
+                        from \{resultTable()}
+                    ) as sub2a
+                    group by athlete_id
+                    order by count desc, athlete_id asc
+                ) as sub2 on sub2.athlete_id = a.athlete_id
+                where a.name is not null
+                order by different_region_course_count desc, total_region_runs desc, a.athlete_id desc
+            """;
+            jdbc.update(sql, new MapSqlParameterSource("minDifferentRegionCourseCount", MIN_DIFFERENT_REGION_COURSE_COUNT));
         }
         catch (DuplicateKeyException ex)
         {
